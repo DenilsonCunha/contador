190bc5e4ba22c1c48543c16ea2a069751f9fbf6c
DenilsonCunha <denilsonluciano6@gmail.com>
2021-05-07 10:14:43 -0300

    3script

--- script.js
+++ script.js
@@ -11,4 +11,14 @@ if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
     let i = Number(ini.value)
     let f = Number(fim.value)
     let p = Number(passo.value)
+    if (p <= 0) {
+        alert ('Passo inválido! Considerando PASSO 1 ')
+        p = 1
+    }
+    if (i < f) {
+        //contagem crescente
+        for (let c = i; c <= f; c +=p) {
+            res.innerHTML += `${c} \u{1f449}`
+        }
+    }
 }
