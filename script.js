5f57b755d850da5d7d17287ea2a5e8648bae90cf
DenilsonCunha <denilsonluciano6@gmail.com>
2021-05-07 10:20:55 -0300

    4script

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function contar() {
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
     let res = document.getElementById('res')
-}
+
 if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
     res.innerHTML = 'impossível contar!'
 } else {
@@ -20,5 +20,11 @@ if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         for (let c = i; c <= f; c +=p) {
             res.innerHTML += `${c} \u{1f449}`
         }
+    } else {
+        //contagem regressiva
+        for (let c = i; c >= f; c -= p) {
+            res.innerHTML += `${c} \u{1f449}`
+      }
     }
+  } res.innerHTML += `\u{1f3c1}`
 }
