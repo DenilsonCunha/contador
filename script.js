4ede42acc725e57ed2073c35a583bef146b6487a
DenilsonCunha <denilsonluciano6@gmail.com>
2021-05-07 09:58:26 -0300

    1script

---
+++ script.js
@@ -0,0 +1,6 @@
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+}
